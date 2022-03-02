5ddd6827c09f4233da2722981e09018b7a8d9e77
Jeffrey Huang <jeffreyh.huang@mail.utoronto.ca>
2022-03-01 23:03:40 -0500

    Removed commented

--- components/MangaImage.js
+++ components/MangaImage.js
@@ -1,22 +1,6 @@
 import { useState } from "react"
 
-function MangaImage() {
-    // const [data, setData] = useState(null)
-    // const [isLoading, setLoading] = useState(false)
-  
-    // useEffect(() => {
-    //   setLoading(true)
-    //   fetch('api/profile-data')
-    //     .then((res) => res.json())
-    //     .then((data) => {
-    //       setData(data)
-    //       setLoading(false)
-    //     })
-    // }, [])
-  
-    // if (isLoading) return <p>Loading...</p>
-    // if (!data) return <p>No profile data</p>
-  
+function MangaImage() {  
     return (
       <div>
         {/* <h1>{data.name}</h1>
