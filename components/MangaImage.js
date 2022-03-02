da02c50738957671c2e7ee01455f80fe58bb4e8a
Jeffrey Huang <jeffreyh.huang@mail.utoronto.ca>
2022-03-01 22:33:20 -0500

    Show images on page

---
+++ components/MangaImage.js
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+function MangaImage() {
+    // const [data, setData] = useState(null)
+    // const [isLoading, setLoading] = useState(false)
+  
+    // useEffect(() => {
+    //   setLoading(true)
+    //   fetch('api/profile-data')
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //       setData(data)
+    //       setLoading(false)
+    //     })
+    // }, [])
+  
+    // if (isLoading) return <p>Loading...</p>
+    // if (!data) return <p>No profile data</p>
+  
+    return (
+      <div>
+        {/* <h1>{data.name}</h1>
+        <p>{data.bio}</p> */}
+        <h1>MangaImageCover</h1>
+      </div>
+    )
+  }
+
+export default MangaImage
